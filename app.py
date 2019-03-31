8b580db8d0dc47d87ede2f73f8c397337f769eab
Szilveszter Erdős <szilveszter.erdos@codecool.com>
2019-03-31 11:09:25 +0200

    fix signing error with adding slackeventsapi module

--- app.py
+++ app.py
@@ -1,62 +1,22 @@
 import os
 import logging
-import hashlib
-import hmac
+from slackeventsapi import SlackEventAdapter
 from flask import abort, Flask, jsonify, request
 
-
+# globals
 app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('lunchbot')
-
-
-def verify_slack_request(slack_signature=None, slack_request_timestamp=None, request_body=None):
-    slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
-
-    # form the basestring as stated in the Slack API docs. We need to make a bytestring
-    basestring = f"v0:{slack_request_timestamp}:{request_body}".encode('utf-8')
-
-    # make the Signing Secret a bytestring too
-    slack_signing_secret = bytes(slack_signing_secret, 'utf-8')
-
-    # create a new HMAC "signature", and return the string presentation
-    my_signature = 'v0=' + hmac.new(slack_signing_secret, basestring, hashlib.sha256).hexdigest()
-
-    # Compare the the Slack provided signature to ours.
-    # If they are equal, the request should be verified successfully.
-    # Log the unsuccessful requests for further analysis
-    # (along with another relevant info about the request).
-    if hmac.compare_digest(my_signature, slack_signature):
-        return True
-    else:
-        logger.warning(f"Verification failed. my_signature: {my_signature}")
-        return False
 
 
 @app.route('/', methods=['POST'])
 def lunchbot():
     try:
-        # capture the necessary data
-        slack_signature = request.headers['X-Slack-Signature']
-        slack_request_timestamp = request.headers['X-Slack-Request-Timestamp']
-
-        # verify the request
-        if not verify_slack_request(slack_signature, slack_request_timestamp, request.data):
-            logger.info('Bad request.')
-            response = {
-                "statusCode": 400,
-                "body": ''
-            }
-            return response
-
         return jsonify(
             response_type='in_channel',
             text='hello there',
         )
     except Exception as e:
         logger.error(f"ERROR: {e}")
-        response = {
-            "statusCode": 200,
-            "body": ''
-        }
-        return response
+        abort(200)
