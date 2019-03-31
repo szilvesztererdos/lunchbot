038e6fe18745faf785606620a09456067a1ab3aa
Szilveszter Erdős <szilveszter.erdos@codecool.com>
2019-03-31 06:44:02 +0200

    change to slash commands

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-from slackclient import SlackClient
-import logging
 import os
-import time
+
+from flask import abort, Flask, jsonify, request
 
 
-def main():
-
-    # 0.1 second delay between reading from firehose
-    READ_WEBSOCKET_DELAY = 0.1
-
-    if slack_client.rtm_connect():
-        logger.info('QuestionBot connected and running!')
-        while slack_client.server.connected is True:
-            print(slack_client.rtm_read())
-            time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        logger.info('Connection failed. Invalid Slack token or bot ID?')
-
-
-# globals
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('lunchbot')
+
+
+def is_request_valid(request):
+    is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
+    is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+
+    return is_token_valid and is_team_id_valid
+
+
+@app.route('/', methods=['POST'])
+def lunchbot():
+    if not is_request_valid(request):
+        abort(400)
+
+    return jsonify(
+        response_type='in_channel',
+        text='hello there',
+    )
 
 if __name__ == "__main__":
     main()
